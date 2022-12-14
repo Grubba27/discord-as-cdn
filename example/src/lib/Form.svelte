6a9734da9c57ea215473aed7f24591103a6308cb
Gabriel Grubba <Grubba27@hotmail.com>
2022-12-13 22:03:58 -0300

    Added example

---
+++ example/src/lib/Form.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let files;
+</script>
+
+<form
+  id="form"
+  on:submit={async (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    const res = await fetch("http://localhost:3000/sendFile", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    console.log(data);
+  }}
+>
+  <input class="input file-input" type="file" name="file" bind:files  />
+  <button class="button" type="submit">Submit</button>
+</form>
